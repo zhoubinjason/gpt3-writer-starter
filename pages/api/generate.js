86000f08f5fbd842b85c1d9cf8aa83a396e4a914
Bin Zhou <binzhou@uwaterloo.ca>
2023-02-13 21:08:03 -0500

    my first draft working

---
+++ pages/api/generate.js
@@ -0,0 +1,49 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+    'Write me a list of daily activities for retirees.\n' +
+     'Things can do for fun, to stay active, and to stay healthy and happy for today.\n' +
+     'This list should be 10 items long.\n' +
+    'Today I want do things like:';
+
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+
+  // I build Prompt #2.
+  const secondPrompt = 'Take the list of daily activities for retirees and write a blog post about it. This blog' +
+    'sounds energetic and fun. It should cover 10 hours of time span including meal time and sleep time';
+
+
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
